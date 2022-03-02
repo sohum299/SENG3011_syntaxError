bc5ec41a5f56f55d70b53466a9fb1f7c694a6b14
Neeraj Mirashi <neeraj@192-168-1-187.tpgi.com.au>
2022-03-02 14:17:24 +1100

    preliminary changes - testing for website output

--- scraper.py
+++ scraper.py
@@ -1,6 +1,24 @@
-import requests
+from time import sleep
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-URL = "https://www.cdc.gov/outbreaks/"
-r = requests.get(URL)
-soup = BeautifulSoup(r.content, 'html5lib')
-print(soup.prettify())
+from selenium.webdriver.support.ui import Select
+
+def scrapeSite(key_terms, location):
+    URL = "http://outbreaks.globalincidentmap.com/"  
+    browser = webdriver.Chrome('C:\chromedriver\chromedriver.exe')
+    browser.get(URL)
+    keys = Select(browser.find_element_by_name("type"))
+    keys.select_by_visible_text(key_terms)
+    try:
+        city = Select(browser.find_element_by_name("country"))
+        city.select_by_visible_text(location)
+    except:  
+        print("no city")
+    browser.find_element_by_name("clicktype").click()
+    with open('rawData.html', 'w') as w:
+        w.write(browser.page_source)
+#def formatScraped(period_of_interest):
+   
+
+scrapeSite("Coronavirus", "AU")
